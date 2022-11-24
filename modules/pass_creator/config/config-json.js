c9ce16d549503c607bec7001d1c557fdaa3e9b81
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2022-11-24 15:48:56 +0100

    Fix typo in config-json

--- modules/pass_creator/config/config-json.js
+++ modules/pass_creator/config/config-json.js
@@ -11,7 +11,7 @@ module.exports = function configJSON(req) {
     lang: {
       'en-GB': {
         name: 'Pass Creator',
-        description: 'WPP Intregration for Pass Creator'
+        description: 'WPP Integration for Pass Creator'
       }
     },
     arguments: {
