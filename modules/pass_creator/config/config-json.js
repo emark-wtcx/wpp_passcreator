7b15b71d59bc5a0c1a3c01c49bc100361f6f0aca
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2022-11-24 16:41:22 +0100

    Update config-json

--- modules/pass_creator/config/config-json.js
+++ modules/pass_creator/config/config-json.js
@@ -19,7 +19,10 @@ module.exports = function configJSON(req) {
         // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
         inArguments: [
           {
-          "emailAddress": "{{InteractionDefaults.Email}}"
+            "contactIdentifier": "{{Contact.Key}}"
+          },
+          {
+          "emailAddress": "{{InteractionDefaults.emailaddress}}"
           },
           {
           "phoneNumber": "{{Contact.Default.PhoneNumber}}"
