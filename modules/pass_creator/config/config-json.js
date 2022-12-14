60cd3b5941e4db495a014c21c6289961d5d739f5
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2022-12-14 15:34:16 +0100

    Refactor config-json

--- modules/pass_creator/config/config-json.js
+++ modules/pass_creator/config/config-json.js
@@ -1,105 +1,93 @@
-module.exports = function configJSON(req) {
-  return {
-    workflowApiVersion: '1.1',
-    metaData: {
-      // the location of our icon file
-      icon: `images/pass_creator.png`,
-      category: 'messages',
-      isConfigured:false,
-      configOnDrop:true
-    },
-    // For Custom Activity this must say, "REST"
-    type: 'REST',
-    // Translation Setup
-    lang: {
-      'en-GB': {
-        name: 'Pass Creator',
-        description: 'WPP Integration for Pass Creator',
-        selectType:'Select Input Type',
-        configureMessage:'Configure Your Message',
-        confirm:'Confirm',
-      }
-    },
-    arguments: {
-      execute: {
-        // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
-        inArguments: [
+return {
+  "workflowApiVersion": "1.1",
+  "metaData": {
+    "icon": `images/pass_creator.png`,
+    "category": 'messages',
+    "isConfigured":false,
+    "configOnDrop":true
+  },
+  "type": "REST",
+  "lang": {
+    "en-US": {
+      "name": 'Pass Creator',
+      "description": 'WPP Integration for Pass Creator',
+      "selectType":'Select Input Type',
+      "configureMessage":'Configure Your Message',
+      "confirm":'Confirm',
+    }
+  },
+  "arguments": {
+    "execute": {
+      "inArguments": [
           {"contactIdentifier": "{{Contact.Key}}"},
           {"emailAddress": "{{InteractionDefaults.Email}}"},
           {"message":""}
         ],
-        outArguments: [],
-        // Fill in the host with the host that this is running on.
-        // It must run under HTTPS
-        url: `https://eol3vy07fc9qzyh.m.pipedream.net`,
-        verb: "POST",
-        method: "POST",
-        format: "json",
-        useJwt: false,
-        // The amount of time we want Journey Builder to wait before cancel the request. Default is 60000, Minimal is 1000
-        timeout: 10000,
-        // how many retrys if the request failed with 5xx error or network error. default is 0
-        retryCount: 3,
-        // wait in ms between retry.
-        retryDelay: 1000,
-        // The number of concurrent requests Journey Builder will send all together
-        concurrentRequests: 5
-      }
-    },
-    configurationArguments: {
-      publish: {
-        url: `https://eon2nxjzthbdt2w.m.pipedream.net`
-      },
-      validate: {
-        url: `https://eoxsr92hcso0n3h.m.pipedream.net`
-      },
-      stop: {
-        url: `https://eoot1xooh8qwfa8.m.pipedream.net`
-      }
-    },
-    wizardSteps: [
-      {
-        "label": "Select Type",
-        "key": "selectType"
-      },
-      {
-        "label": "Configure Message",
-        "key": "configureMessage"
-      },
-      {
-        "label": "Confirm",
-        "key": "confim"
-      },
-    ],    
-    userInterfaces: {
-      configurationSupportsReadOnlyMode : true,
-      configInspector: {
-        size: 'scm-lg',
-        emptyIframe: true
-      }
-    },
-    schema: {
-      arguments: {
-        execute: {
-          inArguments: [
-            {
-              "message":{
-                "dataType": "text",
-                "isNullable": false,
-                "direction": "in"
-                }
-            },
-            {
-              "emailAddress": {
-                "dataType": "Email",
-                "isNullable": false,
-                "direction": "in"
-              }
-            }
-          ],
-          outArguments: []
+      "outArguments": [
+        {
+          "foundSignupDate": ""
         }
+      ],
+      "url": `https://eol3vy07fc9qzyh.m.pipedream.net`,
+      "verb": "POST",
+      "method": "POST",
+      "format": "json",
+      "useJwt": false
+    }
+  },
+  "configurationArguments": {
+    "publish": {
+      "url": `https://eon2nxjzthbdt2w.m.pipedream.net`
+    },
+    "validate": {
+      "url": `https://eoxsr92hcso0n3h.m.pipedream.net`
+    },
+    "stop": {
+      "url": `https://eoot1xooh8qwfa8.m.pipedream.net`
+    }
+  },
+  wizardSteps: [
+    {
+      "label": "Select Type",
+      "key": "selectType"
+    },
+    {
+      "label": "Configure Message",
+      "key": "configureMessage"
+    },
+    {
+      "label": "Confirm",
+      "key": "confim"
+    },
+  ], 
+  "userInterfaces": {
+    "configModal": {
+      "height": 200,
+      "width": 300,
+      "fullscreen": true
+    }
+  },
+  "schema": {
+    "arguments": {
+      "execute": {
+        "inArguments": [
+          {
+            "message":{
+              "dataType": "text",
+              "isNullable": false,
+              "direction": "in"
+              }
+          },
+          {
+            "emailAddress": {
+              "dataType": "Email",
+              "isNullable": false,
+              "direction": "in"
+            }
+          }
+        ],
+        "outArguments": []
       }
     }
-  };
-};
+  }
+}
