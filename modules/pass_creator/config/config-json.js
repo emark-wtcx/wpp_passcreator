8c066a3a7711653c2ac5fa916b4f128cd162018b
Dale McConnell <dale.mcconnell@wundermanthompson.com>
2022-12-19 09:27:46 +0100

    Update config-json Remove prebuild for use with Render

--- modules/pass_creator/config/config-json.js
+++ modules/pass_creator/config/config-json.js
@@ -1,49 +1,53 @@
+module.exports = function configJSON(req) {
 return {
-  "workflowApiVersion": "1.1",
-  "metaData": {
-    "icon": `images/pass_creator.png`,
-    "category": 'messages',
-    "isConfigured":false,
-    "configOnDrop":true
+  workflowApiVersion: '1.1',
+  metaData: {
+    // the location of our icon file
+    icon: `images/pass_creator.png`,
+    category: 'messages',
+    isConfigured:false,
+    configOnDrop:true
   },
-  "type": "REST",
-  "lang": {
-    "en-US": {
-      "name": 'Pass Creator',
-      "description": 'WPP Integration for Pass Creator',
-      "selectType":'Select Input Type',
-      "configureMessage":'Configure Your Message',
-      "confirm":'Confirm',
+  // For Custom Activity this must say, "REST"
+  type: 'REST',
+  // Translation Setup
+  lang: {
+    'en-GB': {
+      name: 'Pass Creator',
+      description: 'WPP Integration for Pass Creator',
+      selectType:'Select Input Type',
+      configureMessage:'Configure Your Message',
+      confirm:'Confirm',
     }
   },
-  "arguments": {
-    "execute": {
-      "inArguments": [
-          {"contactIdentifier": "{{Contact.Key}}"},
-          {"emailAddress": "{{InteractionDefaults.Email}}"},
-          {"message":""}
-        ],
-      "outArguments": [
-        {
-          "foundSignupDate": ""
-        }
+  arguments: {
+    execute: {
+      inArguments: [
+        {"contactIdentifier": "{{Contact.Key}}"},
+        {"emailAddress": "{{InteractionDefaults.Email}}"},
+        {"message":""}
       ],
-      "url": `https://eol3vy07fc9qzyh.m.pipedream.net`,
-      "verb": "POST",
-      "method": "POST",
-      "format": "json",
-      "useJwt": false
+      outArguments: [],
+      url: `https://eol3vy07fc9qzyh.m.pipedream.net`,
+      verb: "POST",
+      method: "POST",
+      format: "json",
+      useJwt: false,
+      timeout: 10000,
+      retryCount: 3,
+      retryDelay: 1000,
+      concurrentRequests: 5
     }
   },
-  "configurationArguments": {
-    "publish": {
-      "url": `https://eon2nxjzthbdt2w.m.pipedream.net`
+  configurationArguments: {
+    publish: {
+      url: `https://eon2nxjzthbdt2w.m.pipedream.net`
     },
-    "validate": {
-      "url": `https://eoxsr92hcso0n3h.m.pipedream.net`
+    validate: {
+      url: `https://eoxsr92hcso0n3h.m.pipedream.net`
     },
-    "stop": {
-      "url": `https://eoot1xooh8qwfa8.m.pipedream.net`
+    stop: {
+      url: `https://eoot1xooh8qwfa8.m.pipedream.net`
     }
   },
   wizardSteps: [
@@ -59,18 +63,18 @@ return {
       "label": "Confirm",
       "key": "confim"
     },
-  ], 
-  "userInterfaces": {
-    "configModal": {
-      "height": 200,
-      "width": 300,
-      "fullscreen": true
+  ],    
+  userInterfaces: {
+    configurationSupportsReadOnlyMode : true,
+    configInspector: {
+      size: 'scm-lg',
+      emptyIframe: true
     }
   },
-  "schema": {
-    "arguments": {
-      "execute": {
-        "inArguments": [
+  schema: {
+    arguments: {
+      execute: {
+        inArguments: [
           {
             "message":{
               "dataType": "text",
@@ -86,8 +90,9 @@ return {
             }
           }
         ],
-        "outArguments": []
+        outArguments: []
       }
     }
   }
-}
+};
+};
